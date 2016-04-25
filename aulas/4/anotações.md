43e3895780e3751f46c3b41108d6ec070c891049
Mean <mean@Alex-Brasileiro.local>
2016-04-24 23:35:03 -0300

    4º Aula INCOMPLETA |  VIDEO - 31m1s

---
+++ aulas/4/anotações.md
@@ -0,0 +1,10 @@
+First Class Citzen || High-Order Functions
+
+* First Class Citizen
+
+	- Assinada a uma variável;
+	- Retornada de uma função;
+	- Ser passada por parâmetro;
+
+# - Vídeo https://youtu.be/zPL6WWpA__o?t=31m1s	
+
---
+++ aulas/4/functions.js
@@ -0,0 +1,68 @@
+// First Class Citizen
+
+	// Assinada a uma variável;
+
+		var hello = function(name) {
+			console.log('Olá ' + name);
+		}
+
+		// hello('Alex');
+
+		// -- //
+
+		var add = function(a,b) {
+			return a + b;
+		}
+
+		console.log(add(400 , 20))
+
+	// Retornada de uma função;	
+
+		var hello2 = function(name) {
+			return function() {
+				console.log('Olá ' + name + ' 2');
+			}
+		}
+
+		var fn = hello2('Alex');
+		// fn();
+
+		// -- //
+
+		function adder(a) {
+			 return function(b) {
+			 	return a + b;
+			 };
+		};
+
+		var _add = adder(20);
+		
+		// console.log(_add(400))
+		// console.log(_add(646))
+
+		// -- //
+
+		function multiply(a) {
+			var sum = 0;
+			return function(b) {
+				sum = b;
+				for(i=1; i<a; i++) {
+					sum += b;
+				}
+				return sum;
+			};
+		}
+
+		console.log( multiply(2)(333) )
+
+	// Ser passada por parâmetro;	
+
+		var hello3 = function(name) {
+			console.log(name);			
+		}
+
+		function ola(msg , name) {
+			msg(name)
+		}
+
+		ola(hello , 'Alex 3')
